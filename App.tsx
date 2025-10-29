85ab87e1ee9932b28bddf98163803a06de54ee63
Shubham Garg <ershubhamgarg@yahoo.in>
2025-10-29 19:59:36 +0530

    removed unused imports from App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
-import { StyleSheet, useColorScheme } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { MainStack } from './src/navigation/MainStack';
-import { AuthProvider } from './src/context/AuthContext';
+import { LogBox, StyleSheet } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { AuthProvider } from "./src/context/AuthContext";
+import { MainStack } from "./src/navigation/MainStack";
 
 function App() {
   return (
